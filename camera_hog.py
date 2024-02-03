1048d34902139a203df61fe9f34161debfdb3cc0
Gildson <37073818+Gildson@users.noreply.github.com>
2024-02-03 20:11:06 -0300

    Add files via upload

--- camera_hog.py
+++ camera_hog.py
@@ -1,18 +1,23 @@
 import cv2
 import dlib
 
+# Criação do modelo
 face_detector = dlib.get_frontal_face_detector()
 
+# Captura da webcam
 video_capture = cv2.VideoCapture(0)
 
 while True:
     try:
         ret, frame = video_capture.read()
 
+        # Transforma cada frame em escala de cinza
         image_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
+        # Detecta as faces
         detections_face = face_detector(frame, 1)
 
+        # Mostra o resultado da detecção em cada frame
         for face in detections_face:
             cv2.rectangle(frame, (face.left(), face.top()), (face.right(), face.bottom()), (0,255,0), 2)
 
